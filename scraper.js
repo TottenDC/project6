ce005200d365dc2143d18cffff7b1754f4be6d3e
Daniel Totten <TottenDC@gmail.com>
2018-11-23 12:10:06 -0500

    Program scrapes all products and inserts into array.
    NOTE: asynchronous right now. Needs to fire completely so I can use the final array.

--- scraper.js
+++ scraper.js
@@ -1,5 +1,6 @@
 //Required modules
 const fs = require('fs');
+const http = require('http');
 const cheerio = require('cheerio');
 const csvStringer = require('csv-stringify');
 
@@ -9,3 +10,38 @@ const csvStringer = require('csv-stringify');
 if (! fs.existsSync('./data')) {
   fs.mkdirSync('./data');
 }
+
+const scrapData = [];
+
+ http.get('http://shirts4mike.com/shirts.php', response => {
+   let html = '';
+   response.on('data', chunk => {
+     html += chunk;
+   }); //end response data event
+   response.on('end', () => {
+     const $home = cheerio.load(html);
+     const links = [];
+     $home('a', '.products').each(function(index, element) {
+       let link = $home(this).attr('href');
+       links.push(link);
+     }); //end each
+     links.forEach(link => {
+       http.get('http://shirts4mike.com/' + link, response =>{
+         let productHTML = '';
+         response.on('data', chunk => {
+           productHTML += chunk;
+         }); //end response data event
+         response.on('end', () =>{
+           const $product = cheerio.load(productHTML);
+           let dataset = [];
+           dataset.push($product('img', '.shirt-picture').attr('alt'));
+           dataset.push($product('.price').text());
+           dataset.push($product('img', '.shirt-picture').attr('src'));
+           dataset.push('http://shirts4mike.com/' + link)
+           dataset.push(new Date());
+           scrapData.push(dataset);
+         }); //end response end event
+       }); //end get method
+     }); //end forEach
+   }); //end response end event
+ }); //end get method
